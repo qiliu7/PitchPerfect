1a02c8e51fa60bda33429e4ed99b57ff3e0afc33
Qi <liuqi@alphae.cn>
2019-10-17 10:23:25 -0600

    fix button icon stretching + add extension

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -7,36 +7,66 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
   
-  @IBOutlet weak var slowButton: UIButton!
+  // MARK: - Outlets
+  @IBOutlet weak var snailButton: UIButton!
+  @IBOutlet weak var rabbitButton: UIButton!
+  @IBOutlet weak var vaderButton: UIButton!
+  @IBOutlet weak var chipmunkButton: UIButton!
+  @IBOutlet weak var echoButton: UIButton!
+  @IBOutlet weak var reverbButton: UIButton!
+  @IBOutlet weak var stopButton: UIButton!
   
-  var recordedAudioURL: URL! {
-    didSet {
-      if let url = recordedAudioURL{
-        print("received URL: \(url)")
-      }
-    }
+  var recordedAudioURL: URL!
+  var audioFile: AVAudioFile!
+  var audioEngine: AVAudioEngine!
+  var audioPlayerNode: AVAudioPlayerNode!
+  var stopTimer: Timer!
+  
+  enum ButtonType: Int {
+    case slow = 0, fast, chipmunk, vader, echo, reverb
   }
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    // Do any additional setup after loading the view.
+    setupAudio()
   }
   
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    // set buttons' image content mode to prevent from stretching
+    let playBackButtons = [snailButton, rabbitButton, vaderButton, chipmunkButton, echoButton, reverbButton]
+    for button in playBackButtons {
+      button?.setToFixAspectRatio()
+    }
+    configureUI(.notPlaying)
+  }
   
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destination.
-   // Pass the selected object to the new view controller.
-   }
-   */
+  // MARK: - Actions
+  @IBAction func playSoundForButton(_ sender: UIButton) {
+    print(" is pressed")
+  }
   
+  @IBAction func stopButtonPressed(_ sender: UIButton) {
+    print("stop button is pressed")
+  }
+
+}
+
+extension UIButton {
+  
+//  convenience init(imageContentMode: UIImageView.ContentMode = .scaleAspectFit) {
+//    self.init()
+//    self.imageView?.contentMode = imageContentMode
+//  }
+  
+  func setToFixAspectRatio() {
+    self.imageView?.contentMode = .scaleAspectFill
+  }
 }
 
 
